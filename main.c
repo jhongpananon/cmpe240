bd747d5ec3485692daf67cedac1c1d678f0e3819
Jonathon Hongpananon <jhongpananon@teslamotors.com>
2017-10-10 19:44:35 -0700

    System project settings

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <8051F120.h>
+#include <c8051F120.h>
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -8,10 +8,22 @@
 
 #define SYSTEM_CLOCK        24500000
 
+// Initialize the system clock
+static void systemClockInit(void);
+
+
+
 int main(void)
 {
+    systemClockInit();
+    
     while(1) 
 	{
         
 	}	
+}
+
+static void systemClockInit(void)
+{
+    OSCICN = 0x83;
 }
