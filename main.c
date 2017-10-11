ea6b7f65c65ef63ca12418bc1f7649017b8373c7
Jonathon Hongpananon <jhongpananon@teslamotors.com>
2017-10-10 19:33:11 -0700

    Add port_io.c and remove bad images

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <8051F120.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
 
+
+#define SYSTEM_CLOCK        24500000
 
 int main(void)
 {
---
+++ port_io.c
@@ -0,0 +1,65 @@
+/////////////////////////////////////
+//  Generated Initialization File  //
+/////////////////////////////////////
+
+#include "compiler_defs.h"
+#include "C8051F120_defs.h"
+
+// Peripheral specific initialization functions,
+// Called from the Init_Device() function
+void Port_IO_Init()
+{
+    // P0.0  -  TX0 (UART0), Open-Drain, Digital
+    // P0.1  -  RX0 (UART0), Open-Drain, Digital
+    // P0.2  -  Unassigned,  Open-Drain, Digital
+    // P0.3  -  Unassigned,  Open-Drain, Digital
+    // P0.4  -  Unassigned,  Open-Drain, Digital
+    // P0.5  -  Unassigned,  Open-Drain, Digital
+    // P0.6  -  Unassigned,  Open-Drain, Digital
+    // P0.7  -  Unassigned,  Open-Drain, Digital
+
+    // P1.0  -  Unassigned,  Open-Drain, Digital
+    // P1.1  -  Unassigned,  Open-Drain, Digital
+    // P1.2  -  Unassigned,  Open-Drain, Digital
+    // P1.3  -  Unassigned,  Open-Drain, Digital
+    // P1.4  -  Unassigned,  Open-Drain, Digital
+    // P1.5  -  Unassigned,  Open-Drain, Digital
+    // P1.6  -  Unassigned,  Open-Drain, Digital
+    // P1.7  -  Unassigned,  Open-Drain, Digital
+
+    // P2.0  -  Unassigned,  Open-Drain, Digital
+    // P2.1  -  Unassigned,  Open-Drain, Digital
+    // P2.2  -  Unassigned,  Open-Drain, Digital
+    // P2.3  -  Unassigned,  Open-Drain, Digital
+    // P2.4  -  Unassigned,  Open-Drain, Digital
+    // P2.5  -  Unassigned,  Open-Drain, Digital
+    // P2.6  -  Unassigned,  Open-Drain, Digital
+    // P2.7  -  Unassigned,  Open-Drain, Digital
+
+    // P3.0  -  Unassigned,  Open-Drain, Digital
+    // P3.1  -  Unassigned,  Open-Drain, Digital
+    // P3.2  -  Unassigned,  Open-Drain, Digital
+    // P3.3  -  Unassigned,  Open-Drain, Digital
+    // P3.4  -  Unassigned,  Open-Drain, Digital
+    // P3.5  -  Unassigned,  Open-Drain, Digital
+    // P3.6  -  Unassigned,  Open-Drain, Digital
+    // P3.7  -  Unassigned,  Open-Drain, Digital
+
+    SFRPAGE   = CONFIG_PAGE;
+    XBR0      = 0x04;
+    XBR2      = 0x40;
+}
+
+void Oscillator_Init()
+{
+    SFRPAGE   = CONFIG_PAGE;
+    OSCICN    = 0x83;
+}
+
+// Initialization function for device,
+// Call Init_Device() from your main program
+void Init_Device(void)
+{
+    Port_IO_Init();
+    Oscillator_Init();
+}
